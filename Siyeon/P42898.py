01ba7610bc15a4afcc38cc664279333b5a310cc1
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2023-01-19 12:13:31 +0900

    O / P / level3 / DP / 등굣길
    좌표 자체가 100까지밖에 안되는 작은 범위니까 이중for문으로 돌리는게 if check도 적고, queue 뺐다 꺼냈다도 적고 훨씬 효율적임

--- Siyeon/P42898.py
+++ Siyeon/P42898.py
@@ -1,49 +1,16 @@
-from queue import Queue
-
-
-def isPuddle(x, y, puddles):
-    for puddle in puddles:
-        print({"puddle": puddle})
-        if puddle[0] == x+1 and puddle[1] == y+1:
-            return True
-    return False
-
-
 def findPath(m, n, puddles):
-    dp = [[0 for i in range(m)] for j in range(n)]
-    dp[0][0] = 1
+    dp = [[0 for i in range(m + 1)] for j in range(n + 1)]
+    dp[1][1] = 1
 
-    que = Queue()
+    for y in range(1, n + 1):
+        for x in range(1, m + 1):
+            if [x, y] in puddles or [x, y] == [1, 1]:
+                continue
+            else:
+                dp[y][x] += dp[y - 1][x] + dp[y][x - 1]
+                dp[y][x] %= 1000000007
 
-    #오른쪽
-    if not isPuddle(1, 0, puddles):
-        que.put((1, 0))
-    # 아래쪽
-    if not isPuddle(0, 1, puddles):
-        que.put((0, 1))
-
-    # 2차배열 안쪽이 x, 바깥이 y
-    while que.qsize() > 0:
-        now = que.get()
-        x = now[0]
-        y = now[1]
-
-        # 왼쪽 부모
-        if x - 1 >= 0:
-            dp[y][x] += dp[y][x - 1]
-            dp[y][x] %= 1000000007
-
-        # 위쪽 부모
-        if y - 1 >= 0:
-            dp[y][x] += dp[y - 1][x]
-            dp[y][x] %= 1000000007
-
-        if x + 1 < m and (x+1, y) not in que.queue and not isPuddle(x+1, y, puddles):
-            que.put((x + 1, y))
-        if y + 1 < n and (x, y+1) not in que.queue and not isPuddle(x, y+1, puddles):
-            que.put((x, y + 1))
-
-    return dp[n - 1][m - 1]
+    return dp[n][m]
 
 
 def solution(m, n, puddles):
