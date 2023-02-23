38df3aff393378edd918533c2ab89ce962b7ba12
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-02-24 02:41:28 +0900

    💚 정답: 백준/실버1/BFS/스타트링크
    아 해냈다

--- Sunny/백준/BFS&DFS/BOJ5014.py
+++ Sunny/백준/BFS&DFS/BOJ5014.py
@@ -11,36 +11,29 @@ from collections import deque
 # 현재 위치 S층
 # U = 현재 + U
 # D = 현재 - D
-# dfs로 풀면 될듯
-
-def bfs():
-    q = deque()
-    q.append((start, 0))
-    while q:
-        current = q.popleft()
-        visited[current[0]] = 1
-        if current[0] == go:
-            result.append(current[1])
-            break
-        if 0 not in visited:
-            break
-        if current[0] + up <= full and visited[current[0] + up] == 0:
-            q.append((current[0] + up, current[1] + 1))
-        if current[0] - down > 0 and visited[current[0] - down] == 0:
-            q.append((current[0] - down, current[1] + 1))
-
 # 100 2 1 1 0
 # 10 1 10 2 1
-full, start, go, up, down = map(int, input().split())
 
+def bfs():
+    q = deque([start])
+    visited[start] = 1
+    while q:
+        current = q.popleft()
+        if current == go:
+            return count[go]
+        if 0 < current + up <= full and visited[current + up] == 0:
+            count[current + up] = count[current] + 1
+            visited[current + up] = 1
+            q.append(current + up)
+        if 0 < current - down <= full and visited[current - down] == 0:
+            count[current - down] = count[current] + 1
+            q.append(current - down)
+            visited[current - down] = 1
+    if count[go] == 0:
+        return "use the stairs"
+
+full, start, go, up, down = map(int, input().split())
 visited = [0 for _ in range(full+1)]
 visited[0] = 1
-result = []
-bfs()
-result.sort()
-
-if len(result) == 0:
-    print("use the stairs")
-else:
-    # print(result)
-    print(result[0])
+count = [0 for _ in range(full+1)]
+print(bfs())
